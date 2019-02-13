d0b1a6f88650a20ad7a433f6a1c1c5cbec84c16b
Gee Hyness <geehyness22@gmail.com>
2019-02-13 23:42:33 +0200

    Restructured the files. Added loader.

---
+++ js/count.js
@@ -0,0 +1,4 @@
+//https://www.googleapis.com/youtube/v3/channels?part=statistics&id=channel_id&key=your_key
+AIzaSyCWoAfyEPA0ZXOC7ktXbeOT3dZosE0yZcA
+UC3qyDawNgO1-C5e22OlShzw
+https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UC3qyDawNgO1-C5e22OlShzw&key=AIzaSyD079WgqhDQoMnBu3pbOreYKhYVkzRo0CY
